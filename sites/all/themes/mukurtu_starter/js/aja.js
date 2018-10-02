ef1c59a69a203e981619dd46a3bbf9e5bb3e0289
Edward Garrett <edward@pinedrop.com>
2018-10-02 22:53:41 +0100

    areas in map without href attribute should be greyed out

--- sites/all/themes/mukurtu_starter/js/aja.js
+++ sites/all/themes/mukurtu_starter/js/aja.js
@@ -1,4 +1,3 @@
 jQuery(document).ready(function($){
-  $('area.inactive').data('maphilight', {fillOpacity: 0.6});
-  $('area.active').data('maphilight', {stroke: true, strokeColor: '000000'});
+  $('area:not[href]').data('maphilight', {fillOpacity: 0.6});
 });
