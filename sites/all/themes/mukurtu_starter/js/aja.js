aaedd2ad246eae3202097f20c9bdaf034d3c32b1
Edward Garrett <edward@pinedrop.com>
2018-10-07 00:52:44 +0100

    move tooltipster tooltips to right of place name

--- sites/all/themes/mukurtu_starter/js/aja.js
+++ sites/all/themes/mukurtu_starter/js/aja.js
@@ -1,4 +1,18 @@
 jQuery(document).ready(function($){
   $('area:not([href])').data('maphilight', {fillOpacity: 0.4});
-  $('area').tooltipster();
+  $('area').tooltipster({
+    side: 'right',
+    delay: 200,
+    distance: 3,
+    trigger: 'custom',
+    triggerOpen: {
+      mouseenter: true,
+      touchstart: true
+    },
+    triggerClose: {
+      click: true,
+      scroll: true,
+      tap: true
+    }
+  });
 });
