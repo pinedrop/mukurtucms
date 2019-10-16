a2478b5055ded81e3bbc1aa4d803b4d3cacf6910
edward garrett <edward@pinedrop.com>
2019-10-16 21:43:29 +0100

    work on display ui for audio navigation

--- sites/all/modules/custom/locale_audio/locale-audio.js
+++ sites/all/modules/custom/locale_audio/locale-audio.js
@@ -2,16 +2,10 @@
 
   Drupal.behaviors.localeAudio = {
     attach: function(context, settings) {
-      console.log('entered attach');
-      $("span").each(function() {
-        console.log($(this).text());
-      });
-      $("div").each(function() {
-        console.log('found div!')
+      $("span[data-locale-audio]").each(function() {
+        console.log('found one!')
       });
     }
   };
-
-  console.log('entered locale audio');
 
 })(jQuery);
