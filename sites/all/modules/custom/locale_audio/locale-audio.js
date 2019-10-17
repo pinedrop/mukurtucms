04d303454b8adc7a6ac864996b7655a836d8c093
edward garrett <edward@pinedrop.com>
2019-10-17 21:34:58 +0100

    add audio playback

--- sites/all/modules/custom/locale_audio/locale-audio.js
+++ sites/all/modules/custom/locale_audio/locale-audio.js
@@ -9,7 +9,6 @@
         $(this).click(function() {
           var $icon = $('[data-locale-audio]', this);
           var $sid = $icon.attr('data-locale-audio');
-          if (!)
           $('#locale-audio-player').load('/locale_audio/atom/'+$sid, function() {
             $('#locale-audio-player').find('audio')[0].play();
             $icon.addClass('fa-spin');
