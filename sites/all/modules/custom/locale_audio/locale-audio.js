e24d3e80eac10a2431d414b193c6e38286336a98
edward garrett <edward@pinedrop.com>
2019-10-17 16:04:27 +0100

    add audio playback

--- sites/all/modules/custom/locale_audio/locale-audio.js
+++ sites/all/modules/custom/locale_audio/locale-audio.js
@@ -3,18 +3,12 @@
   Drupal.behaviors.localeAudio = {
     attach: function(context, settings) {
       if (!$('#locale-audio-player').length) {
-        $("body").append('<div id="locale-audio-player">Bingo bongo</div>');
+        $("body").append('<div id="locale-audio-player"></div>');
       }
       $('span.locale-audio', context).once('locale-audio', function() {
         $(this).click(function() {
           var $sid = $('[data-locale-audio]', this).attr('data-locale-audio');
-          alert($sid);
-          if (!$('#locale-audio-player').length) {
-            alert('not there');
-          }
-          $('#locale-audio-player').load('/locale_audio/atom/'.$sid, function() {
-            alert('did it');
-          });
+          $('#locale-audio-player').load('/locale_audio/atom/'+$sid);
         });
       });
     }
