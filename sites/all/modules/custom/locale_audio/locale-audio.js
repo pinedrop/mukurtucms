3b0edcec8137940be2bd06f9f6e9e5d44ac7e87f
edward garrett <edward@pinedrop.com>
2019-10-18 00:03:52 +0100

    add audio playback

--- sites/all/modules/custom/locale_audio/locale-audio.js
+++ sites/all/modules/custom/locale_audio/locale-audio.js
@@ -8,19 +8,23 @@
       $('span.locale-audio', context).once('locale-audio', function() {
         $(this).click(function() {
           var $icon = $('[data-locale-audio]', this);
-          var $next = $icon.attr('data-locale-audio');
-          var $last = $('#locale-audio-player > div').attr('data-atom-id');
-          if ($next == $last) {
+          var next = $icon.attr('data-locale-audio');
+          var last = $('#locale-audio-player > div').attr('data-atom-id');
+          if (last) {
+            $('[data-locale-audio='+last)[0].classList.remove('fa-spin');
+          }
+          if (next == last) {
             var aud = $('#locale-audio-player').find('audio')[0];
             aud.pause();
             aud.currentTime = 0.0;
             aud.play();
           } else {
-            $('#locale-audio-player').load('/locale_audio/atom/' + $next, function () {
-              $('#locale-audio-player').find('audio')[0].play();
-              $icon[0].classList.remove('fa-podcast');
-              $icon[0].classList.remove('fa-lg');
-              $icon[0].classList.add('fa-circle-notch');
+            $('#locale-audio-player').load('/locale_audio/atom/' + next, function () {
+              var aud = $('#locale-audio-player').find('audio')[0];
+              aud.onended(function() {
+                $icon[0].classList.remove('fa-spin');
+              });
+              aud.play();
               $icon[0].classList.add('fa-spin');
             });
           }
