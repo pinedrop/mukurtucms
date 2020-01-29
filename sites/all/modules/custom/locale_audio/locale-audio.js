ba2d40d8099b12906b64aaa69d2a0dccb24661db
Edward Garrett <edward@pinedrop.com>
2020-01-29 01:40:42 +0000

    use jquery once to improve efficiency

--- sites/all/modules/custom/locale_audio/locale-audio.js
+++ sites/all/modules/custom/locale_audio/locale-audio.js
@@ -5,42 +5,42 @@
       if (!$('#locale-audio-player').length) {
         $("body").append('<div id="locale-audio-player"></div>');
       }
-      findAndReplaceDOMText(document.body, {
-        find: /\uFFF9(.*?)\uFFFA(.*?)\uFFFB/g,
-        replace: function(portion, match) {
-          var wrap = $("<span><span class='locale-audio'><i class='fas fa-lg fa-asterisk' data-locale-audio='" + match[2] + "'></i></span> " + match[1] + "</span>");
-          return wrap[0];
-        }
-        //replace: "<span class='locale-audio'><i class='fas fa-lg fa-asterisk' data-locale-audio='$2'></i></span> $1"
-      });
-      $('span.locale-audio', context).once('locale-audio', function() {
-        $(this).click(function() {
-          var $icon = $('[data-locale-audio]', this);
-          var next = $icon.attr('data-locale-audio');
-          var last = $('#locale-audio-player > div').attr('data-atom-id');
-          var $aud = $('#locale-audio-player').find('audio');
-          if (next == last) {
-            $aud[0].pause();
-            $aud[0].currentTime = 0.0;
-            $aud[0].play();
+      $(context).once('locale-audio', function() {
+        findAndReplaceDOMText(context, {
+          find: /\uFFF9(.*?)\uFFFA(.*?)\uFFFB/g,
+          replace: function (portion, match) {
+            var wrap = $("<span><span class='locale-audio'><i class='fas fa-lg fa-asterisk' data-locale-audio='" + match[2] + "'></i></span> " + match[1] + "</span>");
+            return wrap[0];
           }
-          else {
-            if (last) {
+        });
+        $('span.locale-audio', context).each(function () {
+          $(this).click(function () {
+            var $icon = $('[data-locale-audio]', this);
+            var next = $icon.attr('data-locale-audio');
+            var last = $('#locale-audio-player > div').attr('data-atom-id');
+            var $aud = $('#locale-audio-player').find('audio');
+            if (next == last) {
               $aud[0].pause();
-              $('[data-locale-audio='+last+']')[0].classList.remove('fa-spin');
+              $aud[0].currentTime = 0.0;
+              $aud[0].play();
+            } else {
+              if (last) {
+                $aud[0].pause();
+                $('[data-locale-audio=' + last + ']')[0].classList.remove('fa-spin');
+              }
+              $('#locale-audio-player').load('/locale_audio/atom/' + next, function () {
+                var aud = $('#locale-audio-player').find('audio')[0];
+                aud.onplay = function () {
+                  $('[data-locale-audio=' + next + ']')[0].classList.add('fa-spin');
+                };
+                aud.onended = function () {
+                  $('[data-locale-audio=' + next + ']')[0].classList.remove('fa-spin');
+                };
+                aud.play();
+              });
             }
-            $('#locale-audio-player').load('/locale_audio/atom/' + next, function () {
-              var aud = $('#locale-audio-player').find('audio')[0];
-              aud.onplay = function() {
-                $('[data-locale-audio='+next+']')[0].classList.add('fa-spin');
-              };
-              aud.onended = function() {
-                $('[data-locale-audio='+next+']')[0].classList.remove('fa-spin');
-              };
-              aud.play();
-            });
-          }
-          return false; //stop jquery bubbling
+            return false; //stop jquery bubbling
+          });
         });
       });
     }
